fc6d8952fbcc371816180b58a54a93df8ce2ed14
preethipadukone24 <66549645+preethipadukone24@users.noreply.github.com>
2020-06-11 19:23:24 +0530

    Update Result.cs

--- Modelclass/Result.cs
+++ Modelclass/Result.cs
@@ -1,11 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System;
 
-namespace Modelclass
+namespace Preethi.SunriseSunset
 {
-    public class Class1
+    public sealed class Result
     {
+        [JsonProperty("astronomical_twilight_begin")]
+        public DateTime AstronomicalTwilightStartsAtUtc { get; set; }
+
+        [JsonProperty("astronomical_twilight_end")]
+        public DateTime AstronomicalTwilightEndsAtUtc { get; set; }
+
+        [JsonProperty("civil_twilight_begin")]
+        public DateTime CivilTwilightStartsAtUtc { get; set; }
+
+        [JsonProperty("civil_twilight_end")]
+        public DateTime CivilTwilightEndsAtUtc { get; set; }
+
+        [JsonProperty("day_length")]
+        public int DayLengthInSeconds { get; set; }
+
+        public int DayLengthInMinutes => DayLengthInSeconds / 60;
+
+        public decimal DayLengthInHours => Math.Round(DayLengthInMinutes / 60M, 2);
+
+        [JsonProperty("nautical_twilight_begin")]
+        public DateTime NauticalTwilightStartsAtUtc { get; set; }
+
+        [JsonProperty("nautical_twilight_end")]
+        public DateTime NauticalTwilightEndsAtUtc { get; set; }
+
+        [JsonProperty("solar_noon")]
+        public DateTime SolarNoonAtUtc { get; set; }
+
+        [JsonProperty("sunrise")]
+        public DateTime SunriseAtUtc { get; set; }
+
+        [JsonProperty("sunset")]
+        public DateTime SunsetAtUtc { get; set; }
     }
 }
