3e1b064cd947c0a5c0a61fad030e35daea24fe2d
preethipadukone24 <66549645+preethipadukone24@users.noreply.github.com>
2020-06-11 19:21:13 +0530

    Update Request.cs

--- Modelclass/Request.cs
+++ Modelclass/Request.cs
@@ -1,11 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Modelclass
+namespace Preethi.SunriseSunset.Modelclass
 {
-    public class Class1
+    public sealed class Request
     {
+        public DateTime DateOn { get; set; }
+        public string Endpoint => $"https://api.sunrise-sunset.org/json?lat={Latitude}&lng={Longitude}&date={DateOn:yyyy-MM-dd}&formatted=0";
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
     }
 }
